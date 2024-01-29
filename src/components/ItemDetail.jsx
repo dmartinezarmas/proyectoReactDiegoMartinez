aee1f1772ffe09145ebde847ea157bbeb25e90ee
diego_martinez_armas <134106804+dmartinezarmas@users.noreply.github.com>
2024-01-28 23:39:39 -0300

    creación de componente Item

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,7 +1,21 @@
 const ItemDetail = () => {
 
     return (
-        <h1>Item Detail</h1>
+        <>
+            <h1>Item Detail</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-4">
+                        <img src={item.image} alt={item.title} className="img-fluid" />
+                    </div>
+                    <div className="col-md-4">
+                        <h1>{item.title}</h1>
+                        <h4><b>{item.price}</b></h4>
+                        <h4>{item.description}</h4>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -1,7 +1,25 @@
-const ItemDetailContainer = () => {
+import { useEffect, useState } from "react";
+import ItemDetail from "./ItemDetail";
 
+const ItemDetailContainer = () => {
+    const [item, setItem] = useState([])
+    useEffect(() => {
+        const promesa = new Promise((resolve) => {
+            setTimeout(() => {
+                let producto = arrayProductos.find(item => item.id == 1);
+                resolve(producto);
+            }, 2000);
+        })
+        promesa.then(data => {
+            setItem(data);
+            console.log(data)
+        })
+    }, []);
     return (
-        <h1>Item Detail Container</h1>
+        <>
+            <h1>Item Detail Container</h1>
+            <ItemDetail item = {item} />
+        </>
     )
 }
 
